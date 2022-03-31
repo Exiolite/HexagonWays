8f8cd5fcb62d77b9186da8d928819386dd08722b
Exiolite <kirkirlvov@gmail.com>
2022-03-31 14:21:50 +0300

    Refactoring

--- HexagonWays/Assets/Scripts/System/Grid/GridBoard.cs
+++ HexagonWays/Assets/Scripts/System/Grid/GridBoard.cs
@@ -1,16 +1,57 @@
-﻿using System.Tile;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Tile;
 using UnityEngine;
 
 namespace System.Grid
 {
     public class GridBoard : MonoBehaviour
     {
-        private readonly TileHolder _tileHolder = new TileHolder();
+        private readonly List<HexagonTile> _hexagonTiles = new List<HexagonTile>();
 
         private void Awake()
         {
-            ETile.OnTileStart.AddListener(_tileHolder.AddTile);
-            ETile.OnTileClick.AddListener(_tileHolder.OnTileClicked);
+            ETile.OnTileStart.AddListener(AddTile);
+            ETile.OnTileClick.AddListener(OnTileClicked);
+        }
+
+
+        private void AddTile(HexagonTile hexagonTile)
+        {
+            _hexagonTiles.Add(hexagonTile);
+        }
+
+        private void OnTileClicked(HexagonTile clickedTile)
+        {
+            if (_hexagonTiles.Any(o => o.IsSelected))
+            {
+                var selectedTile = _hexagonTiles.FirstOrDefault(o => o.IsSelected);
+
+                if (selectedTile == clickedTile)
+                {
+                    clickedTile.transform.RotateAround(clickedTile.transform.position, Vector3.up, 60);
+                }
+                else
+                {
+                    var hexagonTilePosition = clickedTile.transform.position;
+                    clickedTile.transform.position = selectedTile.transform.position;
+                    selectedTile.transform.position = hexagonTilePosition;
+                    DeselectAll();
+                }
+            }
+            else
+            {
+                DeselectAll();
+                clickedTile.SelectTile();
+            }
+        }
+
+        private void DeselectAll()
+        {
+            foreach (var tile in _hexagonTiles)
+            {
+                tile.DeselectTile();
+            }
         }
     }
 }
